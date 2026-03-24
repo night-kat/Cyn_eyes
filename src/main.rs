60484ea0676a91aca10916340e10657768962cf6
night-kat <178902183+night-kat@users.noreply.github.com>
2026-03-24 18:31:52 +0100

    Add Yellow ellipse centered
    This (should) be a good starting point. I have finally figured out how
    to make the shape be drawn in the center after days of not getting it.
    The comments are added for help to understand things I didn't understand
    before, but that make sense after the fact

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,40 @@
 //! The simplest possible example that does something.
 #![allow(clippy::unnecessary_wraps)]
 
-use crate::graphics::Color;
 use ggez::{
     Context, GameResult, event,
     glam::*,
-    graphics::{self, DrawParam},
+    graphics::{self, Color},
 };
-use screen_size::get_primary_screen_size;
-
-struct ScreenSize {
-    width: f32,
-    height: f32,
-}
-
-struct EyeProperties {
-    eye_width: f32,
-    eye_height: f32,
-    eye_spacing: f32,
-    eye_top_y: f32,
-    eye_bottom_y: f32,
-}
-
-enum EyeState {
-    Open,
-    Closed,
-}
-
-struct Blinking {
-    open_time: i32,
-    close_time: i32,
-    last_blink_time: i32,
-    current_state: EyeState,
-}
-
-struct Scared {
-    scared: bool,
-    scared_start_time: i32,
-    scared_duration: i32,
-}
 
 struct MainState {
-    left_eye: graphics::Mesh,
-    eye_properties: EyeProperties,
-    screen_size: ScreenSize,
-    solver_image: graphics::Image,
+    ellipse: graphics::Mesh,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let (width, height) = get_primary_screen_size().expect("Screen size");
-        let screen_size = ScreenSize {
-            width: width as f32,
-            height: height as f32,
-        };
-
-        // Declared these outside the struct so i don't need to repeat the calculation
-        let eye_height = (screen_size.height / 5.) * 4.;
-        let eye_y = (screen_size.height / 5.) * 4.;
-
-        let eye_properties = EyeProperties {
-            eye_width: screen_size.width / 3.75 + 20.,
-            eye_height,
-            eye_spacing: (screen_size.width / 6.) * 2.,
-            eye_top_y: eye_y,
-            eye_bottom_y: eye_y + eye_height,
-        };
-
-        let left_eye = graphics::Mesh::new_ellipse(
+        let ellipse = graphics::Mesh::new_ellipse(
             ctx,
             graphics::DrawMode::fill(),
-            vec2(100., 1000.),
-            eye_properties.eye_height,
-            eye_properties.eye_width,
-            1.0,
-            Color {
-                r: 254.,
-                g: 221.,
-                b: 0.,
-                a: 1.,
-            },
+            vec2(0., 0.),
+            100.,
+            150.,
+            2.0,
+            Color::YELLOW,
         )?;
-
-        let solver_image_url = "https://i.quotev.com/g76gnudmst2q.jpg";
-        let img_bytes = match reqwest::blocking::get(solver_image_url) {
-            Ok(x) => match x.bytes() {
-                Ok(bytes) => bytes,
-                Err(_) => panic!("Could not convert to bytes"),
-            },
-            Err(_) => panic!("Error"),
-        };
-
-        let solver_image = crate::graphics::Image::from_bytes(ctx, &img_bytes)?;
-
-        Ok(MainState {
-            left_eye,
-            eye_properties,
-            screen_size,
-            solver_image,
-        })
+        Ok(MainState { ellipse })
+        // let circle = graphics::Mesh::new_circle(
+        //     ctx,
+        //     graphics::DrawMode::fill(),
+        //     // This is basically where the center of the object should be. (0.0, 0.0) means center
+        //     // Positive numbers go up and left, negative numbers down and right
+        //     vec2(0., 0.),
+        //     100.0,
+        //     2.0,
+        //     Color::WHITE,
+        // )?;
+        //
+        // Ok(MainState { circle })
     }
 }
 
@@ -110,9 +46,11 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
-
-        canvas.draw(&self.left_eye, DrawParam::new());
-        // canvas.draw(&self.solver_image, Vec2::new(380., 380.));
+        let dimensions = canvas.screen_coordinates().unwrap();
+        canvas.draw(
+            &self.ellipse,
+            Vec2::new(dimensions.w / 2., dimensions.h / 2.),
+        );
 
         canvas.finish(ctx)?;
 
