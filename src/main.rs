a8fb769e6c2c4b5fa8f1b12eb79ad4fc7b5832f7
night-kat <178902183+night-kat@users.noreply.github.com>
2026-03-26 21:30:50 +0100

    Fix flake.nix
    It would cause an error when creating ggez context but works now thanks
    to adding LD environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 //! The simplest possible example that does something.
 #![allow(clippy::unnecessary_wraps)]
 
-#[derive(Debug)]
-struct EyeProperties {
-    eye_width: f32,
-    eye_height: f32,
-}
 use ggez::{
     Context, GameResult, event,
     glam::*,
@@ -13,60 +8,36 @@ use ggez::{
 };
 
 struct MainState {
-    ellipse: graphics::Mesh,
+    pos_x: f32,
+    circle: graphics::Mesh,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let (w, h) = graphics::GraphicsContext::drawable_size(&ctx.gfx);
-
-        let eye_properties = EyeProperties {
-            eye_width: w * 0.20,
-            eye_height: (h * 0.5),
-        };
-
-        dbg!(&eye_properties);
-
-        let ellipse = graphics::Mesh::new_ellipse(
+        let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
             vec2(0., 0.),
-            eye_properties.eye_width,
-            eye_properties.eye_height,
+            100.0,
             2.0,
-            Color::YELLOW,
+            Color::WHITE,
         )?;
-        Ok(MainState { ellipse })
-        // let circle = graphics::Mesh::new_circle(
-        //     ctx,
-        //     graphics::DrawMode::fill(),
-        //     // This is basically where the center of the object should be. (0.0, 0.0) means center
-        //     // Positive numbers go up and left, negative numbers down and right
-        //     vec2(0., 0.),
-        //     100.0,
-        //     2.0,
-        //     Color::WHITE,
-        // )?;
-        //
-        // Ok(MainState { circle })
+
+        Ok(MainState { pos_x: 0.0, circle })
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
-        let dimensions = canvas.screen_coordinates().unwrap();
 
-        canvas.draw(
-            &self.ellipse,
-            // Place one eye at one quarter of the width (left side) and half the height
-            Vec2::new(dimensions.w * (1. / 4.), dimensions.h / 2.),
-        );
+        canvas.draw(&self.circle, Vec2::new(self.pos_x, 380.0));
 
         canvas.finish(ctx)?;
 
@@ -80,3 +51,4 @@ pub fn main() -> GameResult {
     let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
 }
+
