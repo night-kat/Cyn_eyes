42559783b9c9fa38d852232ccb0fb83eca1d1ad6
night-kat <178902183+night-kat@users.noreply.github.com>
2026-03-30 12:21:54 +0200

    fix: left eye correct position
    I accidentally reverted a commit when fixing flake.nix

--- flake.nix
+++ flake.nix
@@ -10,41 +10,24 @@
     nixpkgs,
   }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    ggezDependencies = with pkgs; [
-      alsa-lib
-      udev
-      libX11
-      libXcursor
-      libXi
-      libXrandr
-      vulkan-loader
-    ];
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      packages = with pkgs;
-        [
-          cargo
-          rustc
-          rustfmt
-          clippy
-          rust-analyzer
-          bacon
-          # pkg-config
-          llvmPackages.clang
-          llvmPackages.lld
-          llvmPackages.libclang
-          cmake
-          gcc
-          expat
-          freetype
-          fontconfig
-        ]
-        ++ ggezDependencies;
+      buildInputs = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+        clippy
+        rust-analyzer
+        bacon
+        alsa-lib
+        systemd # provides libudev
+        libx11
+      ];
 
       nativeBuildInputs = [pkgs.pkg-config];
 
       env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ggezDependencies;
+
       shellHook = ''
         export CARGO_CLIPPY_FLAGS='-- -W clippy::pedantic -W clippy::nursery -W clippy::unwrap_used'
       '';
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 //! The simplest possible example that does something.
 #![allow(clippy::unnecessary_wraps)]
 
+#[derive(Debug)]
+struct EyeProperties {
+    eye_width: f32,
+    eye_height: f32,
+}
 use ggez::{
     Context, GameResult, event,
     glam::*,
@@ -8,36 +13,60 @@ use ggez::{
 };
 
 struct MainState {
-    pos_x: f32,
-    circle: graphics::Mesh,
+    ellipse: graphics::Mesh,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let circle = graphics::Mesh::new_circle(
+        let (w, h) = graphics::GraphicsContext::drawable_size(&ctx.gfx);
+
+        let eye_properties = EyeProperties {
+            eye_width: w * 0.20,
+            eye_height: (h * 0.5),
+        };
+
+        dbg!(&eye_properties);
+
+        let ellipse = graphics::Mesh::new_ellipse(
             ctx,
             graphics::DrawMode::fill(),
             vec2(0., 0.),
-            100.0,
+            eye_properties.eye_width,
+            eye_properties.eye_height,
             2.0,
-            Color::WHITE,
+            Color::YELLOW,
         )?;
-
-        Ok(MainState { pos_x: 0.0, circle })
+        Ok(MainState { ellipse })
+        // let circle = graphics::Mesh::new_circle(
+        //     ctx,
+        //     graphics::DrawMode::fill(),
+        //     // This is basically where the center of the object should be. (0.0, 0.0) means center
+        //     // Positive numbers go up and left, negative numbers down and right
+        //     vec2(0., 0.),
+        //     100.0,
+        //     2.0,
+        //     Color::WHITE,
+        // )?;
+        //
+        // Ok(MainState { circle })
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
+        let dimensions = canvas.screen_coordinates().unwrap();
 
-        canvas.draw(&self.circle, Vec2::new(self.pos_x, 380.0));
+        canvas.draw(
+            &self.ellipse,
+            // Place one eye at one quarter of the width (left side) and half the height
+            Vec2::new(dimensions.w * (1. / 4.), dimensions.h / 2.),
+        );
 
         canvas.finish(ctx)?;
 
@@ -51,4 +80,3 @@ pub fn main() -> GameResult {
     let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
 }
-
