2e4ca98df83be4887da2f28d3ba1e594fc1c8a20
night-kat <178902183+night-kat@users.noreply.github.com>
2026-03-30 13:02:06 +0200

    feat: Eye location improvement

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ struct MainState {
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
         let (w, h) = graphics::GraphicsContext::drawable_size(&ctx.gfx);
-
         let eye_properties = EyeProperties {
             eye_width: w * 0.20,
             eye_height: (h * 0.5),
@@ -30,25 +29,14 @@ impl MainState {
         let ellipse = graphics::Mesh::new_ellipse(
             ctx,
             graphics::DrawMode::fill(),
-            vec2(0., 0.),
+            // Place one eye at one quarter of the width (left side) and half the height
+            vec2(w * (1. / 4.), h / 2.),
             eye_properties.eye_width,
             eye_properties.eye_height,
             2.0,
             Color::YELLOW,
         )?;
         Ok(MainState { ellipse })
-        // let circle = graphics::Mesh::new_circle(
-        //     ctx,
-        //     graphics::DrawMode::fill(),
-        //     // This is basically where the center of the object should be. (0.0, 0.0) means center
-        //     // Positive numbers go up and left, negative numbers down and right
-        //     vec2(0., 0.),
-        //     100.0,
-        //     2.0,
-        //     Color::WHITE,
-        // )?;
-        //
-        // Ok(MainState { circle })
     }
 }
 
@@ -60,13 +48,8 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
-        let dimensions = canvas.screen_coordinates().unwrap();
 
-        canvas.draw(
-            &self.ellipse,
-            // Place one eye at one quarter of the width (left side) and half the height
-            Vec2::new(dimensions.w * (1. / 4.), dimensions.h / 2.),
-        );
+        canvas.draw(&self.ellipse, Vec2::new(0., 0.));
 
         canvas.finish(ctx)?;
 
